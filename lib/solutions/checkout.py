1256a30ac7f119ff42888e84afd2f7236181028f
sbx_user1059 <sbx_user1059@ip-10-12-7-129.eu-west-2.compute.internal>
2018-05-13 20:53:32 +0000

    Sun May 13 20:53:32 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,6 +1,24 @@
 
 
+PRODUCTS = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
+
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    product_count = {
+        "A": 0,
+        "B": 0,
+        "C": 0,
+        "D": 0
+    }
+    for product in skus:
+        product_count[product] += 1
+
+    print(product_count)
+    return 0
