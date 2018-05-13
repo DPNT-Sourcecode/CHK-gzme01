c0cdf9c9904af3a3ff29e50ef926ed2f561cc288
sbx_user1059 <sbx_user1059@ip-10-12-7-129.eu-west-2.compute.internal>
2018-05-13 20:36:31 +0000

    Sun May 13 20:36:31 UTC 2018

---
+++ test/solutions/test_hello_friend.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from lib.solutions.hello import hello
+
+
+class HelloFriendTestCase(TestCase):
+
+    def test_hello(self):
+        self.assertEqual(hello("John"), "Hello, John!")
+        self.assertEqual(hello(""), "Hello, !")
+
+    def test_running(self):
+        self.assertEqual(True, False)
